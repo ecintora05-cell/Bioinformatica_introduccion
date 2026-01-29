ece2eb849b7faf9a26f17ccfc31d542cccfb3858
ecintora05-cell <ecintora05@alumnos.uaq.mx>
2026-01-29 16:54:47 -0600

    Initial commit

--- 02_scripst/intro_1.R
+++ 02_scripst/intro_1.R
@@ -4,7 +4,17 @@ hist(x)
 #para parar lo que se envia en el archivo pdf
 pdf("03_resultados/histograma.pdf")
 hist(x,col="tomato")
-boxplot(x)
+boxplot(x, col="purple")
 dev.off()
 #| eval: false
-usethis::use_git()
+usethis::use_git() #Guarda los cmabios hechos en los scribs, puedes agregar notas
+#desde el comando git-> comit, no se conecta todavía
+usethis::create_github_token()
+#| eval: false
+gitcreds::gitcreds_set()
+#| eval: false
+usethis::use_git_config(
+  user.name = "ecintora05-cell",
+  user.email = "ecintora05@alumnos.uaq.mx")
+#| eval: false
+usethis::use_github(private = TRUE)

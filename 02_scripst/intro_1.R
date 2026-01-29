d2476c558cb712e40f15e3278133ac210c6048f4
Eliseo05-type <cheoperez97@gmail.com>
2026-01-29 16:21:16 -0600

    Initial commit

---
+++ 02_scripst/intro_1.R
@@ -0,0 +1,8 @@
+x<-rnorm(1000)
+hist(x)
+#Se selecciona desde pdf para que se crea y se ocupa el comando dev.off() 
+#para parar lo que se envia en el archivo pdf
+pdf("03_resultados/histograma.pdf")
+hist(x,col="yellow")
+boxplot(x)
+dev.off()
